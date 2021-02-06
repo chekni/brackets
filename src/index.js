9c3e496ddcd424d22848f4d92f80fdd5df336501
chekni <karina_cheshko@mail.ru>
2021-02-06 23:28:58 +0300

    feat: create solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  module.exports = function check(str, bracketsConfig) {
+      const ruleBrackets = new Map(bracketsConfig),
+          stack = [];
+      let expression = str.split('');
+      let unseenedBrackets = expression.length;
+      let currentBracket,
+          pair,
+          stackLength;
+
+      while (unseenedBrackets) {
+          currentBracket = expression.pop();
+          pair = ruleBrackets.get(currentBracket);
+          stackLength = stack.length;
+
+          if ((!stackLength) || (stack[stackLength - 1] != pair)) {
+              stack.push(currentBracket);
+          } else stack.pop();
+
+          unseenedBrackets = expression.length;
+      }
+
+      return !stack.length;
+  }
